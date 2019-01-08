70499b5aaf162d045d989454712161990480eb88
Simon Rowe <srowe@pivotal.io>
2019-01-08 14:57:36 +0000

    clean up and upgrade gradle to 5.1

--- src/main/java/io/pivotal/accounts/AccountsApplication.java
+++ src/main/java/io/pivotal/accounts/AccountsApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
--- src/main/java/io/pivotal/accounts/repository/AccountRepository.java
+++ src/main/java/io/pivotal/accounts/repository/AccountRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-import javax.ws.rs.QueryParam;
 import java.util.List;
 
 public interface AccountRepository extends CrudRepository<Account,Integer> {
